4583b4b111ababf49c6e6c2fdc4b5278819fae60
drplain <gideonobiasor@gmail.com>
2025-03-12 01:03:23 +0100

    Edited my settings database config

--- job_board_backend/job_board_backend/settings.py
+++ job_board_backend/job_board_backend/settings.py
@@ -167,7 +167,7 @@ DATABASES = {
         "NAME": os.getenv("POSTGRES_DB", "test_db"),
         "USER": os.getenv("POSTGRES_USER", "postgres"),
         "PASSWORD": os.getenv("POSTGRES_PASSWORD", "postgres"),
-        "HOST": os.getenv("POSTGRES_HOST", "db"),
+        "HOST": os.getenv("DATABASE_HOST", "db"),
         "PORT": 5432,
     }
 }
